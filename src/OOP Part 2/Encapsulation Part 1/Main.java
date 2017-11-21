5e11118732328c45d119cf45c212147858cedc89
David Jiang <djiang86@binghamton.edu>
2017-11-21 18:25:29 +0000

    added Encapsulation Part 1

---
+++ src/OOP Part 2/Encapsulation Part 1/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    Player player = new Player();
+//        player.name = "David";
+////        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("David", 200, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+    }
+}
