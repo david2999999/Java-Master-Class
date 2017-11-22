039517d3bb0ee5624b27c52b49ada489e03ae30e
David Jiang <djiang86@binghamton.edu>
2017-11-22 13:52:19 +0000

    added setsChallenge part 3

---
+++ src/Collections/Sets & Hashsets/Sets/Sets Challenge Part 3/DwarfPlanet.java
@@ -0,0 +1,7 @@
+
+public class DwarfPlanet extends HeavenlyBody {
+
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
