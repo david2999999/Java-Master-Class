d2ee5e317f1251622dd1bcbaeb955f85c0c2bce0
David Jiang <djiang86@binghamton.edu>
2018-07-14 16:24:43 +0000

    keyword - expressions

---
+++ Basics/KeywordExpressions.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class KeywordExpressions {
+    public static void main(String[] args) {
+
+        double kilometers = (100 * 1.60934);
+
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+
+
+
+
+        int score = 100;
+        if (score > 90) {
+            System.out.println("you got the high score");
+            score = 0;
+        }
+    }
+}
