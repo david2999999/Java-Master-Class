6ef45464070501e8e6b14bbfa940ab5640306854
David Jiang <djiang86@binghamton.edu>
2018-07-14 16:36:35 +0000

    statements

---
+++ Basics/Statements.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Statements {
+    public static void main(String[] args) {
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " + "another " + "still more.");
+
+        int anotherVariable = 50;
+        anotherVariable--;
+        System.out.println("This is another one");
+    }
+}
