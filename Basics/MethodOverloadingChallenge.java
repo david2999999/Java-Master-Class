6e87df2bd14c4c0c601a3d04e7b0b940837672af
David Jiang <djiang86@binghamton.edu>
2018-07-14 20:55:46 +0000

    method overloading challenge

---
+++ Basics/MethodOverloadingChallenge.java
@@ -0,0 +1,33 @@
+package Basics;
+
+public class MethodOverloadingChallenge {
+    public static void main(String[] args) {
+        calcFeetAndInchesToCentimeters(6, 0);
+        calcFeetAndInchesToCentimeters(7, 5);
+
+        calcFeetAndInchesToCentimeters(100);
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || ((inches < 0) && (inches > 12))) {
+            System.out.println("Invalid feet or inches");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " centimeters");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            return -1;
+        }
+
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+}
