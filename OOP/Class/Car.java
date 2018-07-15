63d2d9d69eade6c5bdf2cfc29b6a95ffae0c915e
David Jiang <djiang86@binghamton.edu>
2018-07-15 23:37:52 +0000

    OOP first basic class

---
+++ OOP/Class/Car.java
@@ -0,0 +1,9 @@
+package OOP.Class;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+}
---
+++ OOP/Class/Main.java
@@ -0,0 +1,8 @@
+package OOP.Class;
+
+public class Main {
+    public static void main(String[] args) {
+        Car porche = new Car();
+        Car holden = new Car();
+    }
+}
