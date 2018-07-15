2ae9118379f066fa25fdc2ceb1a29c57fc18c0da
David Jiang <djiang86@binghamton.edu>
2018-07-15 21:40:34 +0000

    user input challenge

---
+++ Basics/challenge/ReadingUserInputChallenge.java
@@ -0,0 +1,36 @@
+package Basics.challenge;
+
+import java.util.Scanner;
+
+public class ReadingUserInputChallenge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+
+                if (counter == 10) {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid Number");
+            }
+
+            scanner.nextLine();
+        }
+
+        System.out.println("sum = " + sum);
+        scanner.close();
+    }
+}
