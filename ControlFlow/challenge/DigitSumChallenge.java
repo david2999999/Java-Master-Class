69c1da6545137d1f811ecb7a0095cad25c089831
David Jiang <djiang86@binghamton.edu>
2018-07-15 17:24:57 +0000

    sum digit challenge

---
+++ ControlFlow/challenge/DigitSumChallenge.java
@@ -0,0 +1,24 @@
+package ControlFlow.challenge;
+
+public class DigitSumChallenge {
+    public static void main(String[] args) {
+        System.out.println(sumDigits(123));
+    }
+
+    private static int sumDigits(int number) {
+        if (number < 10) {
+            return number;
+        }
+
+        int sum = 0;
+
+        while (number > 0) {
+            int digit  = number % 10;
+            sum += digit;
+
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
