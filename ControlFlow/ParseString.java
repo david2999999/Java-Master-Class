13622bcf665bb67a3f2055b4ed4cdf4e61229810
David Jiang <djiang86@binghamton.edu>
2018-07-15 20:20:11 +0000

    parsing numbers

---
+++ ControlFlow/ParseString.java
@@ -0,0 +1,21 @@
+package ControlFlow;
+
+public class ParseString {
+    public static void main(String[] args) {
+        String numberAsString = "2018";
+        System.out.println("numberAsString = " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println(numberAsString);
+        System.out.println(number);
+
+        String doubleAsString = "2018.1235";
+        double doubleNumber = Double.parseDouble(doubleAsString);
+        System.out.println(doubleNumber);
+    }
+}
