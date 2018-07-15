3f0780de8246d2f6e544901bb590ddf7d67b6657
David Jiang <djiang86@binghamton.edu>
2018-07-15 15:35:50 +0000

    do while

---
+++ ControlFlow/DoWhile.java
@@ -0,0 +1,13 @@
+package ControlFlow;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+         do {
+             System.out.println("Count value was " + count);
+             count++;
+         } while (count != 6);
+    }
+}
