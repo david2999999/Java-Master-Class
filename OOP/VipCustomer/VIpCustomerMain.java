ad6d25f5ace01f6201fcb0129e3e95fd6e6c9275
David Jiang <djiang86@binghamton.edu>
2018-07-16 02:02:07 +0000

    constructor challenge

--- OOP/VipCustomer/VIpCustomerMain.java
+++ OOP/VipCustomer/VIpCustomerMain.java
@@ -1,5 +1,15 @@
 package OOP.VipCustomer;
 
 public class VIpCustomerMain {
-    
+    public static void main(String[] args) {
+        VipCustomer customer = new VipCustomer();
+        System.out.println(customer.getName());
+
+        VipCustomer customer1 = new VipCustomer("Lis", 2500);
+        System.out.println(customer1.getName());
+
+        VipCustomer customer2 = new VipCustomer("Tim", 100.00, "tim@gmail.com");
+        System.out.println(customer2.getName());
+        System.out.println(customer2.getEmailAddress());
+    }
 }
