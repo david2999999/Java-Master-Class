ba5f535edb2f88fb28952a18e80a8e0b3024e399
David Jiang <djiang86@binghamton.edu>
2018-07-16 02:41:41 +0000

    inheritance basics

---
+++ OOP/Inheritance/Dog.java
@@ -0,0 +1,18 @@
+package OOP.Inheritance;
+
+public class Dog extends Animal{
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+}
---
+++ OOP/Inheritance/Main.java
@@ -0,0 +1,5 @@
+package OOP.Inheritance;
+
+public class Main {
+    
+}
