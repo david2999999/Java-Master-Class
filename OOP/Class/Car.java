1ae00a5788560da671db523ff3841efaacbce124
David Jiang <djiang86@binghamton.edu>
2018-07-16 00:12:29 +0000

    getter /setters

--- OOP/Class/Car.java
+++ OOP/Class/Car.java
@@ -6,4 +6,18 @@ public class Car {
     private String model;
     private String engine;
     private String colour;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+
+        if (validModel.equals("carrera") || validModel.equals("holden")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
 }
--- OOP/Class/Main.java
+++ OOP/Class/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Car porche = new Car();
         Car holden = new Car();
+
+        porche.setModel("Carrera");
+        System.out.println("Model is " + porche.getModel());
     }
 }
