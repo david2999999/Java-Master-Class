ec4c2693e9533d7a9e1884588fe492d97d99b895
David Jiang <djiang86@binghamton.edu>
2018-07-18 02:59:05 +0000

    composition complete

--- OOP/Composition/Main.java
+++ OOP/Composition/Main.java
@@ -2,6 +2,17 @@ package OOP.Composition;
 
 public class Main {
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
 
+        Monitor theMonitor = new Monitor("27 Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        Motherboard theMotherBoard = new Motherboard("TX-200", "Asus", 4, 6, "v2.123");
+
+        PC thePc = new PC(theCase, theMonitor, theMotherBoard);
+
+        thePc.getMonitor().drawPixelAt(1500, 1200, "Red");
+        thePc.getMotherboard().loadProgram("Window 11");
+        thePc.getTheCase().pressPowerButton();
     }
 }
