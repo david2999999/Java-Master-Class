f3a8ee16300d71900f35689ced482a70a77386e1
David Jiang <djiang86@binghamton.edu>
2018-07-18 00:20:26 +0000

    car class extending vehicle

---
+++ OOP/Inheritance/challenge/Car.java
@@ -0,0 +1,29 @@
+package OOP.Inheritance.challenge;
+
+public class Car extends Vehicle {
+    private int wheels;
+    private int doors;
+    private int geers;
+    private boolean isManual;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int geers, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.geers = geers;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(); changed to " + this.currentGear + " gear.");
+    }
+
+    public void changeVelocity(int speed, int direction) {
+        move(speed, direction);
+        System.out.println("Car.changedVelocity() : Velocity " + speed + " direction " + direction);
+    }
+}
