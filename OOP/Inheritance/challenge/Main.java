27e5509a07217759727209f8af9534512f5afe56
David Jiang <djiang86@binghamton.edu>
2018-07-18 01:14:37 +0000

    inheritance complete

--- OOP/Inheritance/challenge/Main.java
+++ OOP/Inheritance/challenge/Main.java
@@ -2,6 +2,9 @@ package OOP.Inheritance.challenge;
 
 public class Main {
     public static void main(String[] args) {
-
+        Outlander outlander = new Outlander(36);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
     }
 }
