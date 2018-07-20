77cfb244ce47bf76ff89c8a3485ac47d82134bf3
David Jiang <djiang86@binghamton.edu>
2018-07-20 23:43:36 +0000

    reference vs value

---
+++ Basics/ReferenceVsValue.java
@@ -0,0 +1,28 @@
+package Basics;
+
+import java.util.Arrays;
+
+public class ReferenceVsValue {
+    public static void main(String[] args) {
+        int myIntValue = 10;
+        int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        anotherIntValue++;
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        System.out.println("MyIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray = " + Arrays.toString(anotherArray));
+
+        anotherArray[0] = 1;
+        System.out.println();
+        System.out.println("After Change MyIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("After Change anotherArray = " + Arrays.toString(anotherArray));
+    }
+}
