78c90dd7399bb00d14bfdbc945f742642ed56bf1
David Jiang <djiang86@binghamton.edu>
2018-07-20 02:45:01 +0000

    basic array

---
+++ Arrays/Basic/Array.java
@@ -0,0 +1,21 @@
+package Arrays.Basic;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] myIntArray = new int[10];
+
+        for (int i = 0; i < myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+
+        printArray(myIntArray);
+        
+        double[] myDoubleArray = new double[10];
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0 ; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
