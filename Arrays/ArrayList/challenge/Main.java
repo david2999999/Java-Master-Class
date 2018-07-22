51e4f5fdb58921085916fa3fa1a602d22ea2cee2
David Jiang <djiang86@binghamton.edu>
2018-07-22 15:26:33 +0000

    arraylist challenge part 2

--- Arrays/ArrayList/challenge/Main.java
+++ Arrays/ArrayList/challenge/Main.java
@@ -1,7 +1,74 @@
 package Arrays.ArrayList.challenge;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("0039 123 1234");
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while (!quit) {
+            System.out.println("\nEnter actions: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("\nShutting down");
+                    quit = true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+            }
+        }
+    }
+
+    private static void addNewContact() {
+        System.out.println("Enter new contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number");
+        String phone = scanner.nextLine();
+
+        Contact newContact = Contact.createContact(name, phone);
+        if (mobilePhone.addNewContact(newContact)) {
+            System.out.println("New contact added: " + name + ", phone = " + phone);
+        } else {
+            System.out.println("Cannot add, " + name + " already on file");
+        }
+    }
+
+    private static void printActions() {
+        System.out.println("\nAvailable actions: \npress");
+        System.out.println("0 - to shutdown");
+        System.out.println("1 - to print contacts");
+        System.out.println("2 - to add new contact");
+        System.out.println("3 - to update existing contact");
+        System.out.println("4 - to remove an existing contact");
+        System.out.println("5 - query if an existing contact exists");
+        System.out.println("6 - to print a list of available actions");
+        System.out.println("Choose your action: ");
+    }
+
+    private static void startPhone() {
+        System.out.println("Starting phone....");
     }
 }
--- Arrays/ArrayList/challenge/MobilePhone.java
+++ Arrays/ArrayList/challenge/MobilePhone.java
@@ -33,6 +33,18 @@ public class MobilePhone {
         return true;
     }
 
+    public boolean removeContact(Contact contact) {
+        int foundPosition = findContact(contact);
+        if (foundPosition < 0) {
+            System.out.println(contact.getName() + ", was not found.");
+            return false;
+        }
+
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + ", was deleted");
+        return true;
+    }
+
     private int findContact(Contact contact) {
         return this.myContacts.indexOf(contact);
     }
@@ -46,5 +58,19 @@ public class MobilePhone {
         }
 
         return -1;
+    }
+
+    public String queryContact(Contact contact) {
+        if (findContact(contact) >= 0) {
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List");
+        for (int i = 0; i < myContacts.size(); i++) {
+            System.out.println((i + 1) + ". " + this.myContacts.get(i).getName() + " -> " + this.myContacts.get(i).getPhoneNumber());
+        }
     }
 }
