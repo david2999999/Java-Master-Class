dcbd8899b6aa25c0276021fb8aeb63a6d0ede3e5
David Jiang <djiang86@binghamton.edu>
2018-07-22 20:29:44 +0000

    bank class to interact with branch and customer

---
+++ Arrays/Autoboxing/challenge/Bank.java
@@ -0,0 +1,54 @@
+package Arrays.Autoboxing.challenge;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        Branch branch = findBranch(branchName);
+
+        if (branch != null) {
+            return branch.addCustomerTransaction(customerName, amount);
+        }
+
+        return false;
+    }
+
+
+    private Branch findBranch(String branchName) {
+        for (int i = 0; i < branches.size(); i++) {
+            Branch checkedBranch = this.branches.get(i);
+            if (checkedBranch.getName().equals(branchName)) {
+                return checkedBranch;
+            }
+        }
+
+        return null;
+    }
+}
