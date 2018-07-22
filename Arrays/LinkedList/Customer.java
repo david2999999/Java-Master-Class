aaf595ad6f2c8948a2d5563d325bc8c583818381
David Jiang <djiang86@binghamton.edu>
2018-07-22 21:53:47 +0000

    started linked list

---
+++ Arrays/LinkedList/Customer.java
@@ -0,0 +1,27 @@
+package Arrays.LinkedList;
+
+public class Customer {
+    private String name;
+    private Double balance;
+
+    public Customer(String name, Double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
