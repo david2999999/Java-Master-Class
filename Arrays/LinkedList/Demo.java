26605124175118ceddaf9dc2404943611fa33389
David Jiang <djiang86@binghamton.edu>
2018-07-23 00:40:19 +0000

    adding items alphabetically in linked list

--- Arrays/LinkedList/Demo.java
+++ Arrays/LinkedList/Demo.java
@@ -2,26 +2,26 @@ package Arrays.LinkedList;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<>();
-
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
-        placesToVisit.add("Canberra");
-        placesToVisit.add("Adelaide");
-        placesToVisit.add("Darwin");
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit, "Melbourne");
+        addInOrder(placesToVisit, "Brisbane");
+        addInOrder(placesToVisit, "Perth");
+        addInOrder(placesToVisit, "Canberra");
+        addInOrder(placesToVisit, "Adelaide");
+        addInOrder(placesToVisit, "Darwin");
 
         printList(placesToVisit);
 
-        placesToVisit.add(1, "Alice Springs");
+
+        addInOrder(placesToVisit, "Alice Spring");
+        addInOrder(placesToVisit, "Darwin");
         printList(placesToVisit);
 
-        placesToVisit.remove(4);
-        printList(placesToVisit);
     }
 
     private static void printList(LinkedList<String> linkedList) {
@@ -30,5 +30,30 @@ public class Demo {
             System.out.println("Now visiting " + i.next());
         }
         System.out.println("====================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+
+            if (comparison == 0) {
+                // equal, do not add
+                System.out.println(newCity + " is already included as a destination.");
+                return false;
+            } else if (comparison > 0) {
+                // new city should appear before this one
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else {
+                // move on to the next city
+                continue;
+            }
+        }
+
+        stringListIterator.add(newCity);
+        return true;
     }
 }
