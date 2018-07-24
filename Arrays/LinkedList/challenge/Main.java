9568d7229f61447a261da18cc426c838d4b7f5d4
David Jiang <djiang86@binghamton.edu>
2018-07-24 20:46:53 +0000

    base classes for linkedlist challenge

---
+++ Arrays/LinkedList/challenge/Main.java
@@ -0,0 +1,7 @@
+package Arrays.LinkedList.challenge;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Arrays/LinkedList/challenge/Song.java
@@ -0,0 +1,23 @@
+package Arrays.LinkedList.challenge;
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
