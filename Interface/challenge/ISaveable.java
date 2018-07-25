afd19c4bc57bea3b629936cfffe9ed4dfea0ee1a
David Jiang <djiang86@binghamton.edu>
2018-07-25 04:42:03 +0000

    base class to implement the interface

--- Interface/challenge/ISaveable.java
+++ Interface/challenge/ISaveable.java
@@ -1,8 +1,8 @@
 package Interface.challenge;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface ISaveable {
-    ArrayList<String> write();
-    void read(ArrayList<String> savedValues);
+    List<String> write();
+    void read(List<String> savedValues);
 }
---
+++ Interface/challenge/Player.java
@@ -0,0 +1,63 @@
+package Interface.challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values = new ArrayList<>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        values.add(3, this.weapon);
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.strength = Integer.parseInt(savedValues.get(2));
+            this.weapon = savedValues.get(3);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+}
