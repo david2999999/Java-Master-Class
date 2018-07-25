056d6529a1302de90efa598ee135b82073038f58
David Jiang <djiang86@binghamton.edu>
2018-07-25 03:16:13 +0000

    implementing the interface

--- Interface/Main.java
+++ Interface/Main.java
@@ -1,4 +1,12 @@
 package Interface;
 
 public class Main {
+    public static void main(String[] args) {
+        ITelephone myPhone;
+        myPhone = new DeskPhone(123123);
+
+        myPhone.powerOn();
+        myPhone.callPhone(123123);
+        myPhone.answer();
+    }
 }
