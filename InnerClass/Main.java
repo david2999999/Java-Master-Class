65951c689ea8bd7e754cf2fe9ef864197bd2fa8d
David Jiang <djiang86@binghamton.edu>
2018-07-26 01:04:24 +0000

    creating an inner interface

--- InnerClass/Main.java
+++ InnerClass/Main.java
@@ -3,6 +3,19 @@ package InnerClass;
 public class Main {
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
+        mcLaren.addGear(1, 5.3);
+        mcLaren.addGear(2, 10.5);
+        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
 
     }
 }
