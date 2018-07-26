cb074627d7c7396490e7e90bdb9af30f28ae3f97
David Jiang <djiang86@binghamton.edu>
2018-07-26 03:56:09 +0000

    classes that extends abstract class

---
+++ AbstractClass/Bird.java
@@ -0,0 +1,17 @@
+package AbstractClass;
+
+public class Bird extends Animal {
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is pecking");
+    }
+
+    @Override
+    public void breathe() {
+        System.out.println("Bird breathing");
+    }
+}
---
+++ AbstractClass/Main.java
@@ -0,0 +1,13 @@
+package AbstractClass;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Yorkie");
+        dog.breathe();
+        dog.eat();
+
+        Bird bird = new Bird("Parrot");
+        bird.breathe();
+        bird.eat();
+    }
+}
