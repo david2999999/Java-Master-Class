747d0ff682166c9eecc0da06926b520109f21643
David Jiang <djiang86@binghamton.edu>
2018-07-27 01:46:43 +0000

    recursively looping through binary true

---
+++ AbstractClass/challenge/SearchTree.java
@@ -0,0 +1,34 @@
+package AbstractClass.challenge;
+
+public class SearchTree implements NodeList {
+
+    private ListItem root = null;
+
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        if (root != null) {
+            traverse(root.previous());
+            System.out.println(root.getValue());
+            traverse(root.next());
+        }
+    }
+}
