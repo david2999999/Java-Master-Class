a213b9498541789e4d18e15d6d5cc8e1cc9de3b1
David Jiang <djiang86@binghamton.edu>
2018-07-27 02:48:39 +0000

    removing items from binary tree

--- AbstractClass/challenge/Main.java
+++ AbstractClass/challenge/Main.java
@@ -5,7 +5,7 @@ public class Main {
         MyLinkedList list = new MyLinkedList(null);
         list.traverse(list.getRoot());
 
-        String stringData = "5 4 2 1 123 2 3 4 1 2 4 5 2";
+        String stringData = "1 2 3 0 5 4 6 7 8 9";
         String[] data = stringData.split(" ");
         for (String s: data) {
             list.addItem(new Node(s));
