e59f3ef41e46a88a1769c401b67b66e7828f79ef
David Jiang <djiang86@binghamton.edu>
2018-07-27 04:23:17 +0000

    generic team class for players

--- Generic/GenericMain.java
+++ Generic/GenericMain.java
@@ -2,6 +2,15 @@ package Generic;
 
 public class GenericMain {
     public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
     }
 }
