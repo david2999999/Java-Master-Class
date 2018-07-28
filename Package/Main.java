6fc3602674d9109092b8acfa45859ea0d8c3f4a9
David Jiang <djiang86@binghamton.edu>
2018-07-28 21:36:11 +0000

    package demo

---
+++ Package/Main.java
@@ -0,0 +1,9 @@
+package Package;
+
+public class Main {
+    public static void main(String[] args) {
+
+        javafx.scene.Node node = null;
+        org.w3c.dom.Node anotherNode = null;
+    }
+}
---
+++ Package/com/example/awtSample/Main.java
@@ -0,0 +1,8 @@
+package Package.com.example.awtSample;
+
+public class Main {
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Java");
+        myWindow.setVisible(true);
+    }
+}
