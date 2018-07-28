9d63b49b6765d30aa45a9fea0e1bed8aa1dcd9a9
David Jiang <djiang86@binghamton.edu>
2018-07-28 19:29:22 +0000

    generic compareTo method

--- Generic/Team.java
+++ Generic/Team.java
@@ -2,14 +2,13 @@ package Generic;
 
 import java.util.ArrayList;
 
-
 //<T extends Player & Coach & Manager>
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>>{
     private String name;
-    int played = 0;
-    int won = 0;
-    int lost = 0;
-    int tied = 0;
+    private int played = 0;
+    private int won = 0;
+    private int lost = 0;
+    private int tied = 0;
 
     private ArrayList<T> members = new ArrayList<>();
 
@@ -53,5 +52,17 @@ public class Team<T extends Player> {
 
     public int ranking() {
         return won * 2 + tied;
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        if (this.ranking() > team.ranking()) {
+            return -1;
+        } else if (this.ranking() < team.ranking()) {
+            return 1;
+        } else {
+            return 0;
+        }
+
     }
 }
