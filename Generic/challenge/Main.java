7ea45161b541e229406f66bf2806b94a1b2d203a
David Jiang <djiang86@binghamton.edu>
2018-07-28 20:05:09 +0000

    generic challenge complete

--- Generic/challenge/Main.java
+++ Generic/challenge/Main.java
@@ -1,7 +1,21 @@
 package Generic.challenge;
 
+import Generic.FootballPlayer;
+import Generic.Team;
+
 public class Main {
     public static void main(String[] args) {
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crow");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorne = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
 
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(melbourne);
+        footballLeague.add(hawthorne);
+        footballLeague.add(fremantle);
+
+        footballLeague.showLeagueTable();
     }
 }
