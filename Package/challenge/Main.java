3afcb04787987b0a24796d5e7728303fc6698e75
David Jiang <djiang86@binghamton.edu>
2018-07-29 02:20:05 +0000

    package challenge

---
+++ Package/challenge/Main.java
@@ -0,0 +1,17 @@
+package Package.challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.nSum(i));
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.nfactorial(i));
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.fibonacci(i));
+        }
+    }
+}
