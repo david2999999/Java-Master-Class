625e49f111022a56f66f8be8fe947a8ea1b46faf
David Jiang <djiang86@binghamton.edu>
2018-07-29 19:44:50 +0000

    final keyword

---
+++ Final/Main.java
@@ -0,0 +1,14 @@
+package Final;
+
+public class Main {
+    public static void main(String[] args) {
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+        
+    }
+}
---
+++ Final/SomeClass.java
@@ -0,0 +1,18 @@
+package Final;
+
+public class SomeClass {
+    public final int instanceNumber;
+    private static int classCounter = 0;
+    private final String name;
+
+    public SomeClass(String name) {
+        this.name = name;
+        classCounter++;
+        instanceNumber = classCounter;
+        System.out.println(name + " created, instance is " + instanceNumber);
+    }
+
+    public int getInstanceNumber() {
+        return instanceNumber;
+    }
+}
