6182ca2c763f2c78ed867a136ca01351ea662e3d
David Jiang <djiang86@binghamton.edu>
2018-07-29 16:47:24 +0000

    using the static statement

---
+++ Static/Main.java
@@ -0,0 +1,14 @@
+package Static;
+
+public class Main {
+    public static void main(String[] args) {
+        StaticTest firstInstance = new StaticTest("1st Instance");
+        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+        StaticTest secondInstance = new StaticTest("2nd Instance");
+        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+        StaticTest thirdInstance = new StaticTest("3rd Instance");
+        System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+    }
+}
