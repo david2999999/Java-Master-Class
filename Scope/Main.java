2351f1dfcf213e38be8e843a989b54ed4e3b4af7
David Jiang <djiang86@binghamton.edu>
2018-07-29 04:25:56 +0000

    simple scope demo

---
+++ Scope/Main.java
@@ -0,0 +1,13 @@
+package Scope;
+
+public class Main {
+    public static void main(String[] args) {
+        String privateVar = "This is private to main()";
+
+        ScopeCheck scopeCheck = new ScopeCheck();
+        System.out.println("scopeCheck privateVar is " + scopeCheck.getPrivateVar());
+        System.out.println(privateVar);
+
+        scopeCheck.timesTwo();
+    }
+}
