3dc24d30822c472814747c28339544bfa9bdc668
David Jiang <djiang86@binghamton.edu>
2018-07-29 21:28:14 +0000

    collections complete

--- Collections/Main.java
+++ Collections/Main.java
@@ -2,6 +2,19 @@ package Collections;
 
 public class Main {
     public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        theatre.getSeats();
 
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Seat is already taken");
+        }
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Seat is already taken");
+        }
     }
 }
