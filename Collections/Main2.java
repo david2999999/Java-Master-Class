a8d3555dc5f137c7c4a9f2423e678990bc8491d9
David Jiang <djiang86@binghamton.edu>
2018-07-30 00:31:32 +0000

    reversing list with collections

---
+++ Collections/Main2.java
@@ -0,0 +1,36 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
+
+        seatCopy.get(1).reserve();
+        if (theatre.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
+        } else {
+            System.out.println("Seat already reserved");
+        }
+
+        Collections.reverse(seatCopy);
+        System.out.println("Printing seatCopy");
+        printList(seatCopy);
+        System.out.println("Printing theatre.seat");
+        printList(theatre.seats);
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat: list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+
+        System.out.println();
+        System.out.println("=============================");
+    }
+}
+
