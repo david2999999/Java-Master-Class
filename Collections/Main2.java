4fe0e4f611d42ff57303333893a1c398b3528ec4
David Jiang <djiang86@binghamton.edu>
2018-07-30 00:42:41 +0000

    deep copy with collection.copy

--- Collections/Main2.java
+++ Collections/Main2.java
@@ -31,6 +31,9 @@ public class Main2 {
         sortList(seatCopy);
         System.out.println("Printing sorted seatCopy");
         printList(seatCopy);
+
+        List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
+        Collections.copy(newList, theatre.seats);
     }
 
     public static void printList(List<Theatre.Seat> list) {
