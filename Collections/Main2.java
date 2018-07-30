434e4b78b877dc61c6b6aff61d11b93c670374f8
David Jiang <djiang86@binghamton.edu>
2018-07-30 00:41:01 +0000

    sorting with double for loop and also collection.swap

--- Collections/Main2.java
+++ Collections/Main2.java
@@ -17,11 +17,20 @@ public class Main2 {
             System.out.println("Seat already reserved");
         }
 
-        Collections.reverse(seatCopy);
+        Collections.shuffle(seatCopy);
         System.out.println("Printing seatCopy");
         printList(seatCopy);
         System.out.println("Printing theatre.seat");
         printList(theatre.seats);
+
+        Theatre.Seat minSeat = Collections.min(seatCopy);
+        Theatre.Seat maxSeat = Collections.max(seatCopy);
+        System.out.println("Min seat number is " + minSeat.getSeatNumber());
+        System.out.println("Max seat number is " + maxSeat.getSeatNumber());
+
+        sortList(seatCopy);
+        System.out.println("Printing sorted seatCopy");
+        printList(seatCopy);
     }
 
     public static void printList(List<Theatre.Seat> list) {
@@ -31,6 +40,16 @@ public class Main2 {
 
         System.out.println();
         System.out.println("=============================");
+    }
+
+    public static void sortList(List<? extends Theatre.Seat> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j))> 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
     }
 }
 
