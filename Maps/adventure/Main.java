d3a7f3d0f6da25772735ffdd292e1558e8d45c2e
David Jiang <djiang86@binghamton.edu>
2018-07-31 03:33:39 +0000

    map challenge complete

--- Maps/adventure/Main.java
+++ Maps/adventure/Main.java
@@ -22,21 +22,23 @@ public class Main {
         locations.get(1).addExit("E", 3);
         locations.get(1).addExit("S", 4);
         locations.get(1).addExit("N", 5);
-        locations.get(1).addExit("Q", 0);
 
         locations.get(2).addExit("N", 5);
-        locations.get(2).addExit("Q", 0);
 
         locations.get(3).addExit("W", 1);
-        locations.get(3).addExit("Q", 0);
 
         locations.get(4).addExit("N", 1);
         locations.get(4).addExit("W", 2);
-        locations.get(4).addExit("Q", 0);
 
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
-        locations.get(5).addExit("Q", 0);
+
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
 
 
         int loc = 1;
@@ -46,8 +48,27 @@ public class Main {
                 break;
             }
 
-            loc = scanner.nextInt();
-            if (!locations.containsKey(loc)) {
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are " );
+            for (String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word: words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
