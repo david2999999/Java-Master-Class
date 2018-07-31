52ec561c02de64402b2020990cd83224b23524ed
David Jiang <djiang86@binghamton.edu>
2018-07-31 02:43:41 +0000

    adding exits to locations

--- Maps/adventure/Main.java
+++ Maps/adventure/Main.java
@@ -17,6 +17,28 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in a forest"));
 
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+        locations.get(5).addExit("Q", 0);
+
+
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
