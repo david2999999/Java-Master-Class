45f14e2b32e768b8bb44caa09c039bc25567baae
David Jiang <djiang86@binghamton.edu>
2018-08-01 01:05:10 +0000

    creating immutable classes

---
+++ ImmutableClasses/Location.java
@@ -0,0 +1,29 @@
+package ImmutableClasses;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description, Map<String, Integer> exits) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<>(exits);
+        this.exits.put("Q", 0);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<>(exits);
+    }
+}
