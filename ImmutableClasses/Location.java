75af4094e837f830c326952824e08cf4aa620fe3
David Jiang <djiang86@binghamton.edu>
2018-08-01 01:11:40 +0000

    fixed null pointer exception in location class

--- ImmutableClasses/Location.java
+++ ImmutableClasses/Location.java
@@ -11,7 +11,11 @@ public class Location {
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>(exits);
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
