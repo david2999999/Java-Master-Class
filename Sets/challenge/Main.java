d8cde448737b3a8e3dd0b4b1f51b6dbc6a0e220f
David Jiang <djiang86@binghamton.edu>
2018-08-02 01:22:54 +0000

    equal and hash code challenge complete

--- Sets/challenge/Main.java
+++ Sets/challenge/Main.java
@@ -2,6 +2,10 @@ package Sets.challenge;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Labrador rover = new Labrador("Rover");
+        Dog rover2 = new Dog("Rover");
+
+        System.out.println(rover2.equals(rover));
+        System.out.println(rover.equals(rover2));
     }
 }
