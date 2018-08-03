160a6eef5d0b54d9c6d10271629d5cac1027b330
David Jiang <djiang86@binghamton.edu>
2018-08-03 21:42:47 +0000

    created basket class for stockitems

---
+++ SortedCollection/Basket.java
@@ -0,0 +1,36 @@
+package SortedCollection;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Basket {
+    private final String name;
+    private final Map<StockItem, Integer> list;
+
+    public Basket(String name) {
+        this.name = name;
+        this.list = new HashMap<>();
+    }
+
+    public int addToBasket(StockItem item, int quantity) {
+        if ((item != null) && quantity > 0) {
+            int inBasket = list.getOrDefault(item, 0);
+            list.put(item, inBasket + quantity);
+            return inBasket;
+        }
+        return 0;
+    }
+
+    public Map<StockItem, Integer> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        return "Basket{" +
+                "name='" + name + '\'' +
+                ", list=" + list +
+                '}';
+    }
+}
