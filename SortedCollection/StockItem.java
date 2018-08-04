06ec4a4a753eea50ee2084cc30534889f800ee60
David Jiang <djiang86@binghamton.edu>
2018-08-04 14:19:18 +0000

    sort collection challenge reserving stockitems

--- SortedCollection/StockItem.java
+++ SortedCollection/StockItem.java
@@ -4,6 +4,7 @@ public class StockItem implements Comparable<StockItem> {
     private final String name;
     private double price;
     private int quantityStock = 0;
+    private int reserved = 0;
 
     public StockItem(String name, double price) {
         this.name = name;
@@ -26,7 +27,7 @@ public class StockItem implements Comparable<StockItem> {
     }
 
     public int quantityInStock() {
-        return quantityStock;
+        return quantityStock - reserved;
     }
 
     public void setPrice(double price) {
@@ -40,6 +41,22 @@ public class StockItem implements Comparable<StockItem> {
         if (newQuantity >= 0) {
             this.quantityStock += newQuantity;
         }
+    }
+
+    public int reserveStock(int quantity) {
+        if (quantity <= quantityInStock()) {
+            reserved += quantity;
+            return quantity;
+        }
+        return 0;
+    }
+
+    public int unreserveStock(int quantity) {
+        if (quantity <= reserved) {
+            reserved -= quantity;
+            return quantity;
+        }
+        return 0;
     }
 
     @Override
