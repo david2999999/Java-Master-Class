072d34da7b61f624712cd6d75b9ac11a26d39bdc
David Jiang <djiang86@binghamton.edu>
2018-08-05 20:33:22 +0000

    defining ui with fxml

--- JavaFX/Basic/Main.java
+++ JavaFX/Basic/Main.java
@@ -16,19 +16,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
-
-        Label greeting = new Label("Welcome to Javafx");
-        greeting.setTextFill(Color.GREEN);
-        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-        root.getChildren().add(greeting);
         primaryStage.show();
+
+
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//        Label greeting = new Label("Welcome to Javafx");
+//        root.getChildren().add(greeting);
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
     }
 
 
