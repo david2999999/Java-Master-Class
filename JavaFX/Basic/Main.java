3fa0a96eeefc92919af3615b9e9705834e985c79
David Jiang <djiang86@binghamton.edu>
2018-08-05 19:42:50 +0000

    creating label and added to gridpane

--- JavaFX/Basic/Main.java
+++ JavaFX/Basic/Main.java
@@ -2,17 +2,27 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+        primaryStage.setTitle("Hello JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+        Label greeting = new Label("Welcome to Javafx");
+        root.getChildren().add(greeting);
         primaryStage.show();
     }
 
