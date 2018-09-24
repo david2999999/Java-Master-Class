39b5a94ce184f3f286d869e9c7ad08a7e994f84f
David Jiang <djiang86@binghamton.edu>
2018-09-24 13:32:05 +0000

    using thread.sleep to give other thread a chance to run first

--- Threads/Demo/ThreadDemo.java
+++ Threads/Demo/ThreadDemo.java
@@ -2,12 +2,14 @@ package Threads.Demo;
 
 import static Threads.Demo.ThreadColor.ANSI_GREEN;
 import static Threads.Demo.ThreadColor.ANSI_PURPLE;
+import static Threads.Demo.ThreadColor.ANSI_RED;
 
 public class ThreadDemo {
     public static void main(String[] args) {
         System.out.println(ANSI_PURPLE + "Hello from the main thread.");
-        Thread anotherThread = new AnotherThread();
 
+        Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
         new Thread() {
@@ -16,7 +18,12 @@ public class ThreadDemo {
             }
         }.start();
 
-        Thread myRunnable = new Thread(new MyRunnable());
+        Thread myRunnable = new Thread(new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+            }
+        });
         myRunnable.start();
 
         System.out.println(ANSI_PURPLE + "Hello again from the main thread");
