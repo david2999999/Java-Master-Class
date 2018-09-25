dbd06ac46e2811760cf945f797b5d19bee49b7f5
David Jiang <djiang86@binghamton.edu>
2018-09-25 00:10:58 +0000

    preventing deadlocks by giving locks in order

--- Threads/Deadlock/Main.java
+++ Threads/Deadlock/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static Object lock2 = new Object();
 
     public static void main(String[] args) {
-
+        new Thread1().start();
+        new Thread2().start();
     }
 
     private static class Thread1 extends Thread {
@@ -31,22 +32,22 @@ public class Main {
 
     private static class Thread2 extends Thread {
         public void run() {
-            synchronized (lock2) {
-                System.out.println("Thread 2: has lock2");
+            synchronized (lock1) {
+                System.out.println("Thread 2: has lock1");
 
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {}
 
-                System.out.println("Thread 2: waiting for lock1");
+                System.out.println("Thread 2: waiting for lock2");
 
-                synchronized (lock1) {
-                    System.out.println("Thread 1: Has lock2 and lock1");
+                synchronized (lock2) {
+                    System.out.println("Thread 2: Has lock1 and lock2");
                 }
-                System.out.println("Thread 1: Released lock1");
+                System.out.println("Thread 2: Released lock2");
             }
 
-            System.out.println("Thread 1: released lock2");
+            System.out.println("Thread 2: released lock1");
         }
     }
 }
