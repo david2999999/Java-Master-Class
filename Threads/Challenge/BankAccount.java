90b6ff15e56526c4b5f22cee06f07291771e5225
David Jiang <djiang86@binghamton.edu>
2018-09-26 01:47:14 +0000

    simple bank account for challenge

---
+++ Threads/Challenge/BankAccount.java
@@ -0,0 +1,19 @@
+package Threads.Challenge;
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+}
