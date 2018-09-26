1d5f820fc207fdb1ee063eb00cb2ee504cbbdf03
David Jiang <djiang86@binghamton.edu>
2018-09-26 01:54:49 +0000

    making 2 transactions with threads

---
+++ Threads/Challenge/Main.java
@@ -0,0 +1,24 @@
+package Threads.Challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        final BankAccount account = new BankAccount("1231-12312", 1000.00);
+
+        Thread thread1 = new Thread() {
+            public void run() {
+                account.deposit(300.00);
+                account.withdraw(50.00);
+            }
+        };
+
+        Thread thread2 = new Thread() {
+            public void run() {
+                account.deposit(203.75);
+                account.withdraw(100.00);
+            }
+        };
+
+        thread1.start();
+        thread2.start();
+    }
+}
