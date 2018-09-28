485ed2734dbf82214fc62b6699a5475830321542
David Jiang <djiang86@binghamton.edu>
2018-09-28 03:28:56 +0000

    using lock instead of synch

--- Threads/Challenge/BankAccount.java
+++ Threads/Challenge/BankAccount.java
@@ -1,12 +1,18 @@
 package Threads.Challenge;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private String accountNumber;
     private double balance;
 
+    private Lock lock;
+
     public BankAccount(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
+        this.lock = new ReentrantLock();
     }
 
 //    public synchronized void deposit(double amount) {
@@ -18,16 +24,43 @@ public class BankAccount {
 //    }
 
 
-    public void deposit(double amount) {
-        synchronized (this) {
-            balance += amount;
-        }
+//    public void deposit(double amount) {
+//        synchronized (this) {
+//            balance += amount;
+//        }
+//
+//    }
+//
+//    public void withdraw(double amount) {
+//        synchronized (this) {
+//            balance -= amount;
+//        }
+//    }
 
+    public void deposit(double amount) {
+        lock.lock();
+        try {
+            this.balance += amount;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void withdraw(double amount) {
-        synchronized (this) {
-            balance -= amount;
+        lock.lock();
+
+        try {
+            this.balance -= amount;
+        } finally {
+            lock.unlock();
         }
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account Number = " + accountNumber);
     }
 }
